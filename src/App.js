2660f376016b5cc94e23f72e605714b91cf6080a
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-10 15:57:16 +0200

    feat: add flipping

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
         });
         resetTurn();
       } else {
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
@@ -70,7 +70,12 @@ function App() {
 
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
