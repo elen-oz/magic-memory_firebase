574cbaa1b19ab9c1bd0ce06f3d71fb12f6dfabba
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-10 15:12:24 +0200

    feat: add comparing card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
@@ -30,8 +30,28 @@ function App() {
   // handle a choice
   const handleChoice = (card) => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
-    console.log('choiceOne', choiceOne);
-    console.log('choiceTwo', choiceTwo);
+    // console.log('choiceOne', choiceOne);
+    // console.log('choiceTwo', choiceTwo);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log('cards MATCH');
+        resetTurn();
+      } else {
+        console.log('cards DO NOT match');
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
   };
 
   return (
