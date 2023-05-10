088121d1afd9137c658f936b1488f1fed08eca69
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-10 22:48:11 +0200

    feat: add cards disable

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './SingleCard.css';
 
-const SingleCard = ({ card, handleChoice, flipped }) => {
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
   const handleClick = () => {
-    handleChoice(card);
+    if (!disabled) {
+      handleChoice(card);
+    }
   };
+
   return (
     <div className='card'>
       <div className={flipped ? 'flipped' : ''}>
